71c7c1eec6612f1f5509e5ada7a5aa3eb00fff72
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-02-02 23:16:02 +0530

    First Task Done.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import NasaPhoto from "./Components/NasaPhoto";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/nasaphoto" element={<NasaPhoto />}></Route>
+
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { parsePath } from "react-router-dom";
+
+export default function NasaPhoto() {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    fetchPhoto();
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=Mc5pPqnjhk88JlUcvYswQubphubVlOZHkhCCbaa1`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+      console.log(data);
+    }
+  }, []);
+
+  if (!photoData) return <div />;
+
+  return (
+    <div>
+      <img src={photoData.url} alt={photoData.title} />
+      <div>
+        <h1>{photoData.title}</h1>
+        <h4>{photoData.date}</h4>
+        <p>{photoData.explanation}</p>
+      </div>
+    </div>
+  );
+}
