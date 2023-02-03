47d453dd7df8a8e6a32aed37f8ed383cef75e3a6
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-02-03 20:31:17 +0530

    Code Cleaned.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import NasaPhoto from "./Components/NasaPhoto";
-import Login from './Components/Login';
-import Signup from './Components/Signup';
 import './App.css';
 
 function App() {
@@ -12,8 +9,6 @@ function App() {
       <Routes>
 
       <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup />}></Route>
       <Route path="/nasaphoto" element={<NasaPhoto />}></Route>
 
         
