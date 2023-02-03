c41ef09b327debfb519f4f5f8a9d1919a53505db
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-02-04 00:44:09 +0530

    App Working.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import logo from './logo.svg';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import NasaPhoto from "./Components/NasaPhoto";
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[user,setLoginUser] = useState({})
+
   return (
     <BrowserRouter>
       <Routes>
-
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={
+        user && user._id 
+        ?
+        <Home/> 
+         :
+        <Login  setLoginUser={setLoginUser}/>
+      }></Route>
+      <Route path="/login" element={<Login/>} setLoginUser={setLoginUser}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
       <Route path="/nasaphoto" element={<NasaPhoto />}></Route>
-
-        
       </Routes>
     </BrowserRouter>
   );
